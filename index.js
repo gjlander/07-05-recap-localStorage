0e57a4cff02e452167240379ac13cdcebb80e57e
gjlander <garrett.jlander@gmail.com>
2024-05-08 15:19:20 +0200

    added some extra comments to cover details

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 //accepts two arguments key, value
 // localStorage.setItem('myThing', "Look Ma, I've been set in localstorage!");
 
-// //using same key will override existing key
+// //using same key will override existing item
 // localStorage.setItem('myThing', "I've been overwritten!");
 
 // //getItem()
-// //one argument: key returns: value
+// //one argument: key, returns: value
 // const myNewThing = localStorage.getItem('myThing');
 // console.log('value of newThing: ', myNewThing);
 
@@ -29,6 +29,7 @@
 // }, 2000);
 
 //undefined
+//JSON does not accept undefined as a value, it will cause an error
 // let somethingUndefined;
 // let somethingNull = null;
 
@@ -107,20 +108,35 @@ const newPersonForm = document.querySelector('#new-person-form');
 const peopleList = document.querySelector('.people-list');
 
 const handleAddPerson = (e) => {
+    //needed only on submit events, prevents default behavior of refreshing the page
     e.preventDefault();
+
+    //grab inputs so we can read their value
     const nameInput = document.querySelector('#name');
     const ageInput = document.querySelector('#age');
+
+    //get our array or people from localStorage, or declare an empty array so we can still use our array methods
     const storedPeople = JSON.parse(localStorage.getItem('people')) || [];
+
+    //our person to be added, with random id assigned, and name and age coming from input values
     const newPerson = {
         id: crypto.randomUUID(),
         name: nameInput.value,
         age: ageInput.value,
     };
+
+    //add new person to our array
     storedPeople.push(newPerson);
+
+    //update localStorage
     localStorage.setItem('people', JSON.stringify(storedPeople));
     console.log(storedPeople);
+
+    //clear inputs
     nameInput.value = '';
     ageInput.value = '';
+
+    //create a new li, give it text, and add it to our ul to display on UI
     const personLi = document.createElement('li');
     personLi.innerText = `${newPerson.name} is ${newPerson.age} years old`;
     peopleList.appendChild(personLi);
@@ -131,8 +147,12 @@ newPersonForm.addEventListener('submit', handleAddPerson);
 //display with  UI from initial value
 
 const showPeopleOnLoad = () => {
+    //not using array methods, and want to know if it is truthy, so no logical or
     const storedPeople = JSON.parse(localStorage.getItem('people'));
+
+    //check if storedPeople is truthy (arrays are truthy)
     if (storedPeople) {
+        //if storedPeople exists, iterate over it and create our UI for each person
         storedPeople.forEach((person) => {
             const personLi = document.createElement('li');
             personLi.innerText = `${person.name} is ${person.age} years old`;
@@ -140,4 +160,6 @@ const showPeopleOnLoad = () => {
         });
     }
 };
+
+//call the function so it will run on page load
 showPeopleOnLoad();
